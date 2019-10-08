3d16b01870e4c07e02167746cc946eb4d499eb57
Anton Obzhirov <a.obzhirov@samsung.com>
2019-10-08 18:28:35 +0100

    Use ANDROID_NDK and ANDROID_SDK exported in env for build scripts.

--- dali/build_core.sh
+++ dali/build_core.sh
@@ -8,7 +8,7 @@ cd ./dali-core/cmake/${TARGET}
 if [ -z "$ENABLE_TRACE" ]; then
 export ENABLE_TRACE=OFF
 fi
-~/Android/Sdk/cmake/3.10.2.4988404/bin/cmake ../../../../../dali-core/build/tizen -DCMAKE_C_COMPILER=~/Android/Sdk/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64/bin/clang -DCMAKE_CXX_COMPILER=~/Android/Sdk/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64/bin/clang++ -DCMAKE_INSTALL_PREFIX=../../../dali-core/ -DCMAKE_TOOLCHAIN_FILE=~/Android/Sdk/ndk-bundle/build/cmake/android.toolchain.cmake -DANDROID_PLATFORM=24 -DANDROID_ABI=${TARGET} -DANDROID_NDK=${HOME}/Android/Sdk/ndk-bundle -DANDROID_STL=c++_shared -DCMAKE_CXX_FLAGS='-fexceptions -frtti -w -Wall -std=gnu++11' -DINCLUDE_DIR=../include -DLIB_DIR=lib/${TARGET} -DCMAKE_BUILD_TYPE=$@ -DENABLE_TRACE=${ENABLE_TRACE}
+${ANDROID_SDK}/cmake/3.10.2.4988404/bin/cmake ../../../../../dali-core/build/tizen -DCMAKE_C_COMPILER=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/clang -DCMAKE_CXX_COMPILER=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/clang++ -DCMAKE_INSTALL_PREFIX=../../../dali-core/ -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK}/build/cmake/android.toolchain.cmake -DANDROID_PLATFORM=24 -DANDROID_ABI=${TARGET} -DANDROID_NDK=${ANDROID_NDK} -DANDROID_STL=c++_shared -DCMAKE_CXX_FLAGS='-fexceptions -frtti -w -Wall -std=gnu++11' -DINCLUDE_DIR=../include -DLIB_DIR=lib/${TARGET} -DCMAKE_BUILD_TYPE=$@ -DENABLE_TRACE=${ENABLE_TRACE}
 fi
 make
 make install
